e62a66e41d526395aa5cfae80ee9159122d06576
Carlos Eduardo <cb5224144@gmail.com>
2019-09-20 11:02:07 -0300

    Novos Exe

---
+++ 100/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _100_Primeiros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+        
+            while (x <= 100)
+            {
+                if (x % 2 != 0)
+                
+                { 
+                Console.WriteLine("{0}",x);
+                }
+
+            x++;
+            }
+        
+            
+        }
+    }
+}
---
+++ Par/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Par
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            string func;
+
+            
+            do{
+
+            Console.WriteLine("Digite um numero");
+            x = int.Parse (Console.ReadLine());
+
+            if (x % 2 == 0){
+                Console.WriteLine("Par");
+
+            }else{
+                Console.WriteLine("Impar");
+                }
+
+            Console.WriteLine("(1) para ir novamente ou (f) para sair");
+            func = Console.ReadLine();}
+
+            while(func == "1"); 
+        }
+    }
+}
