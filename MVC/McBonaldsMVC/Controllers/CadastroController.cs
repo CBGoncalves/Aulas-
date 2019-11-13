238f2a41155a741f28e40915e8005a1bbec3393c
Carlos Eduardo <cb5224144@gmail.com>
2019-11-13 11:23:49 -0300

    Login/Pedidos McBonalds

--- MVC/McBonaldsMVC/Controllers/CadastroController.cs
+++ MVC/McBonaldsMVC/Controllers/CadastroController.cs
@@ -1,4 +1,5 @@
 using McBonalds_MVC.Models;
+using McBonaldsMVC.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -6,6 +7,8 @@ namespace McBonalds_MVC.Controllers
 {
     public class CadastroController : Controller
     {
+
+        ClienteRepository clienteRepositorio = new ClienteRepository();
         public IActionResult Index()
         {
             return View();
@@ -15,10 +18,15 @@ namespace McBonalds_MVC.Controllers
         {
             ViewData["Action"] = "Cadastro";
             try{
-            Cliente cliente = new Cliente(form["nome"], form["endereco"], form["telefone"], form["senha"], form["email"], DateTime.Parse(form["data-nascimento"]));
-            System.Console.WriteLine(form["nome"]);
-            return View("Sucesso");
-            }catch(Exception e){
+                Cliente cliente = new Cliente(form["nome"], form["endereco"], form["telefone"], form["senha"], form["email"], DateTime.Parse(form["data-nascimento"]));
+                System.Console.WriteLine(form["nome"]);
+
+                clienteRepositorio.Inserir(cliente);
+
+                return View("Sucesso");
+            
+            }
+            catch(Exception e){
                 return View ("Erro"); 
             }
         }
